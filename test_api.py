15731552418a6fc1121ef6d10b8202367aee4d5b
mathewspmathew <mathewsparan@gmail.com>
2025-08-26 00:42:26 +0530

    Remove secret.env from tracking

--- test_api.py
+++
@@ -1,9 +0,0 @@
-import google.generativeai as genai
-import os
-
-# Configure the client with your API key
-genai.configure(api_key="AIzaSyDhg-F-xHLo-55SR0Eh2KlS9TXDrOKMuLQ")
-
-# List all available models and print their names
-for model in genai.list_models():
-    print(model.name)
